47688e0e2e59b28e5fb048a1fa2936aa757136ba
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2020-12-21 15:50:33 +0530

    This is the Form. No animation!!!

---
+++ UnderstandingWPF/Projects_in_WPF/UI_forms/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheBasicform
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Description Says:{ DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked = this.LaserCheckBox.IsChecked = this.PurchaseCheckBox.IsChecked = this.LatheCheckBox.IsChecked =
+                this.FoldCheckBox.IsChecked = this.DrillCheckBox.IsChecked = this.RollCheckBox.IsChecked = this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LenghthBox.Text  += ((CheckBox)sender).Content;
+
+        }
+    }
+}
---
+++ UnderstandingWPF/Projects_in_WPF/UI_forms/readme.txt
@@ -0,0 +1,5 @@
+In this Module I learned how to make any UI{form Like} in WPF. Here I used Nested Stackpanel, Grid and other XAML tags to make a form. 
+I am putting the screenshot as well in the repo.
+Thanks for having a look. 
+
+
